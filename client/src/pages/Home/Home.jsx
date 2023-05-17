c050a4ad692a267e86be7f78df2f3b49c43dd449
sherlyastha <asthapandeycse9@gmail.com>
2023-05-17 18:04:32 +0530

    ft-categories

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Slider from '../../components/Slider/Slider';
 import "./Home.scss";
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts';
+import Categories from '../../components/Categories/Categories';
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
     <div className='home'>
       <Slider/>
       <FeaturedProducts   type="Featured"/>
+      <Categories/>
       <FeaturedProducts   type="Trending"/>
     </div>
   )
